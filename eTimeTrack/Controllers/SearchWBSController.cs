6b2285e998ae3c8a15f3417b007301b64a08510b
RajaM2 <manirathnam.raja@aecom.com>
2023-08-10 14:33:46 +0530

    Added Search WBS

--- eTimeTrack/Controllers/SearchWBSController.cs
+++ eTimeTrack/Controllers/SearchWBSController.cs
@@ -1,4 +1,6 @@
 ﻿using eTimeTrack.Helpers;
+using eTimeTrack.Models;
+using eTimeTrack.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,26 @@ namespace eTimeTrack.Controllers
         {
             int selectedProject = (int?)Session?["SelectedProject"] ?? 0;
             if (selectedProject == 0) { return InvokeHttp404(HttpContext); }
-            return View();
+            var results = from p in Db.ProjectVariationItems
+                          join t in Db.ProjectTasks on p.TaskID equals t.TaskID
+                          join v in Db.ProjectVariations on p.VariationID equals v.VariationID
+                          where t.ProjectID == selectedProject
+                          select new SearchWBSViewModel
+                          {
+                              VarItemIsClosed = p.IsClosed,
+                              VariationIsClosed = v.IsClosed,
+                              TaskIsClosed = t.IsClosed,
+                              TaskName = t.Name,
+                              TaskNo = t.TaskNo,
+                              VariationName = v.Description,
+                              VariationNo = v.VariationNo
+                          };
+            var res = results.ToList();
+            return View(new ShowSearchWPSViewModel
+            {
+                searchWPS = res
+            });
         }
+        
     }
 }
---
+++ eTimeTrack/ViewModels/SearchWBSViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using eTimeTrack.Models;
+
+namespace eTimeTrack.ViewModels
+{
+    public class SearchWBSViewModel
+    {
+
+        public bool VarItemIsClosed { get; set; }
+        public bool TaskIsClosed { get; set; }
+        public bool VariationIsClosed { get; set; }
+        public string TaskNo { get; set; }
+        public string TaskName { get; set; }
+        public string VariationNo { get; set; }
+        public string VariationName { get; set; }
+
+    }
+
+    public class ShowSearchWPSViewModel
+    {
+        public List<SearchWBSViewModel> searchWPS {  get; set; }
+        public bool HideClosed { get; set; }
+    }
+}
