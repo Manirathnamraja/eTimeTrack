e83d7748f8f02024bea987ec75590884335e1a50
RajaM2 <manirathnam.raja@aecom.com>
2023-08-16 14:59:05 +0530

    Added Employee Inputs time sheets

---
+++ eTimeTrack/Controllers/EmployeeInputsTimesheetController.cs
@@ -0,0 +1,21 @@
+﻿using eTimeTrack.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eTimeTrack.Controllers
+{
+    [Authorize(Roles = UserHelpers.AuthTextUserAdministratorOrAboveExcludeTimesheetEditor)]
+    public class EmployeeInputsTimesheetController : BaseController
+    {
+        [Authorize(Roles = UserHelpers.AuthTextUserPlusOrAbove)]
+        public ActionResult Index()
+        {
+            int selectedProject = (int?)Session?["SelectedProject"] ?? 0;
+            if (selectedProject == 0) { return InvokeHttp404(HttpContext); }
+            return View();
+        }
+    }
+}
