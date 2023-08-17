0aae6001c96e225a6000adc7da1d348243000488
RajaM2 <manirathnam.raja@aecom.com>
2023-08-17 21:44:08 +0530

    Added employee time sheet migrations

---
+++ eTimeTrack/Migrations/202308171610401_EmployeeTimesheetItems.cs
@@ -0,0 +1,24 @@
+﻿namespace eTimeTrack.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeTimesheetItems : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.EmployeeTimesheetItems", "IsTimeSheetApproval", c => c.Boolean());
+            AddColumn("dbo.EmployeeTimesheetItems", "Reviewercomments", c => c.String());
+            AddColumn("dbo.EmployeeTimesheetItems", "LastApprovedBy", c => c.Int());
+            AddColumn("dbo.EmployeeTimesheetItems", "LastApprovedDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.EmployeeTimesheetItems", "LastApprovedDate");
+            DropColumn("dbo.EmployeeTimesheetItems", "LastApprovedBy");
+            DropColumn("dbo.EmployeeTimesheetItems", "Reviewercomments");
+            DropColumn("dbo.EmployeeTimesheetItems", "IsTimeSheetApproval");
+        }
+    }
+}
