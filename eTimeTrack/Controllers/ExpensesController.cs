8bf3d62e64e4ccae0180640587d9673d9a5a3be4
RajaM2 <manirathnam.raja@aecom.com>
2023-08-28 20:49:32 +0530

    Expenses chanages

--- eTimeTrack/Controllers/ExpensesController.cs
+++ eTimeTrack/Controllers/ExpensesController.cs
@@ -1,4 +1,7 @@
-﻿using eTimeTrack.Helpers;
+﻿using Elmah.ContentSyndication;
+using eTimeTrack.Helpers;
+using eTimeTrack.Models;
+using eTimeTrack.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +19,54 @@ namespace eTimeTrack.Controllers
         {
             int selectedProject = (int?)Session?["SelectedProject"] ?? 0;
             if (selectedProject == 0) { return InvokeHttp404(HttpContext); }
-            return View();
+            ExpensesTypesViewModel expensesTypesViewModel = new ExpensesTypesViewModel();
+            var result = (from e in Db.ProjectExpenseTypes
+                         join t in Db.ProjectTasks on e.TaskID equals t.TaskID
+                         join v in Db.ProjectVariations on e.VariationID equals v.VariationID
+                         where t.ProjectID == selectedProject
+                         select new ExpensesTypes
+                         {
+                             ExpenseTypeID = e.ExpenseTypeID,
+                             ExpenseType = e.ExpenseType,
+                             TaskID = e.TaskID,
+                             VariationID = e.VariationID,
+                             Name = t.Name,
+                             Description = v.Description,
+                             ProjectId = t.ProjectID
+                         }).ToList();
+
+            // SetViewbag(result);
+            foreach (var item in result)
+            {
+                expensesTypesViewModel.Tasks = Gettask(item.ProjectId, item.Name);
+                expensesTypesViewModel.variations = Getvariations(item.ProjectId, item.Description);
+
+            }
+            return View(new ExpensesTypesViewModel
+            {
+                ExpensesTypesDetails = result,
+                Tasks = expensesTypesViewModel.Tasks,
+                variations = expensesTypesViewModel.variations
+            }); 
+        }
+
+        private SelectList Gettask(int projectId, string name)
+        {
+            return new SelectList(GetProjectTaskdetails(projectId), "TaskID", "Name", name);
+        }
+        private SelectList Getvariations(int projectId, string name)
+        {
+            return new SelectList(GetProjectVariationdetails(projectId), "VariationID", "Description", name);
+        }
+        private List<ProjectTask> GetProjectTaskdetails(int? projectId)
+        {
+            var deta =  Db.ProjectTasks.Where(x => x.ProjectID == projectId).ToList();
+            return deta;
+        }
+       
+        private List<ProjectVariation> GetProjectVariationdetails(int? projectId)
+        {
+            return Db.ProjectVariations.Where(x => x.ProjectID == projectId).ToList();
         }
     }
 }
--- eTimeTrack/ViewModels/ExpensesTypesViewModel.cs
+++ eTimeTrack/ViewModels/ExpensesTypesViewModel.cs
@@ -3,23 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace eTimeTrack.ViewModels
 {
     public class ExpensesTypesViewModel
     {
-        public ExpensesTypes ExpensesTypesDetails { get; set; }
+        public List<ExpensesTypes> ExpensesTypesDetails { get; set; }
+        public SelectList Tasks { get; set; }
+        public SelectList variations { get; set; }
     }
 
     public class ExpensesTypes
     {
         public int ExpenseTypeID { get; set; }
-        public int TaskId { get; set; }
-        public int VariationId { get; set; }
         public int ProjectId { get; set; }
         public string ExpenseType { get; set; }
-        public ProjectTask DefaultTask { get; set; }
-        public ProjectVariation DefaultVariation { get; set; }
+        public int TaskID { get; set; }
+        public string Name { get; set; }
+        public int VariationID { get; set; }
+        public string Description { get; set; }
 
     }
 }
