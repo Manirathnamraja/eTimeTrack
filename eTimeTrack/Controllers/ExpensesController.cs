4476f51f900db852f23a99cf9c210ff93a3db295
RajaM2 <manirathnam.raja@aecom.com>
2023-08-29 13:37:44 +0530

    completed Expenses types

--- eTimeTrack/Controllers/ExpensesController.cs
+++ eTimeTrack/Controllers/ExpensesController.cs
@@ -5,6 +5,7 @@ using eTimeTrack.ViewModels;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.RefAndLookup;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +15,6 @@ namespace eTimeTrack.Controllers
     [Authorize(Roles = UserHelpers.AuthTextUserAdministratorOrAboveExcludeTimesheetEditor)]
     public class ExpensesController : BaseController
     {
-
         [Authorize(Roles = UserHelpers.AuthTextUserPlusOrAbove)]
         public ActionResult ExpensesTypes()
         {
@@ -53,8 +53,54 @@ namespace eTimeTrack.Controllers
                 Db.ProjectExpenseTypes.Remove(expensesTypes);
                 Db.SaveChanges();
             }
-            TempData["InfoMessage"] = new InfoMessage(InfoMessageType.Success, "Succesfully deleted item");
+            TempData["InfoMessage"] = new InfoMessage(InfoMessageType.Success, "Succesfully deleted Expenses Types");
             return RedirectToAction("ExpensesTypes");
+        }
+
+        public ActionResult Create()
+        {
+            int selectedProject = (int?)Session?["SelectedProject"] ?? 0;
+            ProjectExpenseType projectExpense = new ProjectExpenseType();
+            projectExpense.ProjectID = selectedProject;
+
+            SetViewbag(projectExpense.ProjectID);
+            ViewBag.Source = Source.Create;
+            return View("CreateEdit", projectExpense);
+        }
+
+        public ActionResult edit(int? id)
+        {
+            ProjectExpenseType projectExpense = Db.ProjectExpenseTypes.Find(id);
+            SetViewbag(projectExpense.ProjectID);
+            ViewBag.Source = Source.Existing;
+            return View("CreateEdit", projectExpense);
+        }
+
+        [HttpPost]
+        public ActionResult CreateEdit(ProjectExpenseType projectExpenseType, Source source)
+        {
+            ProjectExpenseType existing = Db.ProjectExpenseTypes.Find(projectExpenseType.ExpenseTypeID);
+            projectExpenseType.LastModifiedBy = UserHelpers.GetCurrentUserId();
+            projectExpenseType.LastModifiedDate = DateTime.UtcNow;
+            if(existing != null)
+            {
+                Db.Entry(existing).CurrentValues.SetValues(projectExpenseType);
+                Db.Entry(existing).State = EntityState.Modified;
+            }
+            else
+            {
+                Db.ProjectExpenseTypes.Add(projectExpenseType);
+            }
+            Db.SaveChanges();
+            TempData["InfoMessage"] = new InfoMessage { MessageContent = $"Project Expense Type {projectExpenseType.ExpenseType}  Saved.", MessageType = InfoMessageType.Success };
+            return RedirectToAction("ExpensesTypes");
+        }
+
+        private void SetViewbag(int? projectId)
+        {
+            ViewBag.ProjectID = new SelectList(Db.Projects, "ProjectID", "Name");
+            ViewBag.TaskID = new SelectList(GetProjectTaskdetails(projectId), "TaskID", "Name");
+            ViewBag.VariationID = new SelectList(GetProjectVariationdetails(projectId), "VariationID", "Description");
         }
 
         private SelectList Gettask(int projectId, string name)
