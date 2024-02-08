ee9b1fe0e9ec77814aac6c3faa4952e71135a3ca
Manirathnam Raja <Manirathnam.Raja@aecom.com>
2024-02-08 16:10:10 +0530

    report

---
+++ eTimeTrack/Controllers/ProjectExpenseReportController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eTimeTrack.Helpers;
+using eTimeTrack.Models;
+using eTimeTrack.ViewModels;
+
+namespace eTimeTrack.Controllers
+{
+    [Authorize(Roles = UserHelpers.AuthTextUserAdministratorOrAboveExcludeTimesheetEditor)]
+    public class ProjectExpenseReportController : BaseController
+    {
+        [Authorize(Roles = UserHelpers.AuthTextUserPlusOrAbove)]
+        public ActionResult Index()
+        {
+            int projectId = (int?)Session["SelectedProject"] ?? 0;
+            Project project = Db.Projects.Find(projectId) ?? Db.Projects.OrderBy(x => x.ProjectNo).First();
+
+            if (project == null)
+            {
+                return InvokeHttp404(HttpContext);
+            }
+
+            ExportRatesIndexUserVm vm = new ExportRatesIndexUserVm { ProjectList = GenerateDropdownUserProjects() };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public FileResult PrintFriendlyExcelData(int projectId)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var results = context.Database.SqlQuery<ProjectExpenseReportViewModel>("select * from dbo.vw_MET_ProjectExpenses");
+
+                byte[] bytes = System.IO.File.ReadAllBytes("");
+
+                var date = DateTime.Now.ToString("yyyyMMddHHmmss");
+                string filename = $"UserRates_{""}_{date}.xlsx";
+
+                return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename);
+            }
+        }
+    }
+}
---
+++ eTimeTrack/ViewModels/ProjectExpenseReportViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eTimeTrack.ViewModels
+{
+    public class ProjectExpenseReportViewModel
+    {
+
+        public string InvoiceNumber { get; set; }
+
+        public string TransactionID { get; set; }
+
+        public string ExpenseDate { get; set; }
+
+        public string CostedInWeekEnding { get; set; }
+
+        public string Cost { get; set; }
+
+        public string HomeOfficeType { get; set; }
+
+        public string EmployeeSupplierName { get; set; }
+
+        public string ExpenditureComment { get; set; }
+
+        public string ProjectComment { get; set; }
+
+        public string IsFeeRecovery { get; set; }
+
+        public string IsCostRecovery { get; set;}
+
+        public string Completed { get; set; }
+
+        public string ExpenseType { get; set;}
+
+        public string VariationNo { get; set;}
+
+        public string Description { get; set;}
+
+        public string TaskNo { get; set;}
+
+        public string Name { get; set;}
+
+        public string EmployeeNo { get; set;}
+
+        public string TravellerName { get; set;}
+
+        public string ProjectName { get; set;}
+
+        public string Company_Name { get; set;}
+
+    }
+}
