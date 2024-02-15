93595a652fdfda3673987323bd172225b758dbca
Manirathnam Raja <Manirathnam.Raja@aecom.com>
2024-02-15 09:26:17 +0530

    upload issue fixed

--- eTimeTrack/Controllers/ExpensesUploadController.cs
+++ eTimeTrack/Controllers/ExpensesUploadController.cs
@@ -201,7 +201,7 @@ namespace eTimeTrack.Controllers
                             var expensestypes = context.ProjectExpenseTypes.Where(x => x.ProjectID == model.ProjectId).FirstOrDefault();
 
                             bool existexpenses = context.ProjectExpensesUploads.Any(x => x.TransactionID == transactionIDdetails);
-                            if (!existexpenses && !string.IsNullOrEmpty(idevalues))
+                            if (!existexpenses)
                             {
                                 ProjectExpensesUpload expenses = new ProjectExpensesUpload
                                 {
