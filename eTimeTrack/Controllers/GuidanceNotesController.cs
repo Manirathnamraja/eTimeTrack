c034469baedbce37103ad5cf3b87a9a6742af410
Manirathnam Raja <Manirathnam.Raja@aecom.com>
2024-02-19 15:37:19 +0530

    create guidance

---
+++ eTimeTrack/Controllers/GuidanceNotesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eTimeTrack.Helpers;
+using eTimeTrack.Models;
+
+namespace eTimeTrack.Controllers
+{
+    [Authorize(Roles = UserHelpers.AuthTextUserPlusOrAbove)]
+    public class GuidanceNotesController : BaseController
+    {
+        public ActionResult Index()
+        {
+            int ProjectId = (int?)Session?["SelectedProject"] ?? 0;
+
+            return View(new ProjectGuidanceNotes
+            {
+                ProjectId = ProjectId
+            });
+        }
+    }
+}
--- eTimeTrack/Models/ProjectGuidanceNotes.cs
+++ eTimeTrack/Models/ProjectGuidanceNotes.cs
@@ -12,19 +12,33 @@ namespace eTimeTrack.Models
         [Key]
         public int GuidanceNoteId { get; set; }
         public int ProjectId { get; set; }
+        [Display(Name = "Time Administrator contact")]
         public string TimeAdministratorContact { get; set; }
+        [Display(Name = "Online notes used with WBS codes")]
         public string WBSCodes { get; set; }
+        [Display(Name = "Cap weekly hours at")]
         public string CapWeeklyHours { get; set; }
+        [Display(Name = "Match hours to home office timesheet?")]
         public string MatchHours  { get; set; }
+        [Display(Name = "Overtime Approval requirements")]
         public string OvertimeApproval  { get; set; }
+        [Display(Name = "NT Time Code Note")]
         public string NTTimeCodeNote { get; set; }
+        [Display(Name = "OT1 Time Code Note")]
         public string OT1TimeCodeNote { get; set; }
+        [Display(Name = "OT2 Time Code Note")]
         public string OT2TimeCodeNote { get; set; }
+        [Display(Name = "OT3 Time Code Note")]
         public string OT3TimeCodeNote { get; set; }
+        [Display(Name = "OT4 Time Code Note")]
         public string OT4TimeCodeNote { get; set; }
+        [Display(Name = "OT5 Time Code Note")]
         public string OT5TimeCodeNote { get; set; }
+        [Display(Name = "OT6 Time Code Note")]
         public string OT6TimeCodeNote { get; set; }
+        [Display(Name = "OT7 Time Code Note")]
         public string OT7TimeCodeNote { get; set; }
+        [Display(Name = "Timesheet Comments")]
         public string TimesheetComments { get; set; }
         public int? LastModifiedBy { get; set; }
         public DateTime? LastModifiedDate { get; set; }
