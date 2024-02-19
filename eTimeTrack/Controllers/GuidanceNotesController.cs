08a06a6f46a1e894a9d4c6d6194fd825c8a5c9bc
Manirathnam Raja <Manirathnam.Raja@aecom.com>
2024-02-19 15:45:34 +0530

    Saved completed

--- eTimeTrack/Controllers/GuidanceNotesController.cs
+++ eTimeTrack/Controllers/GuidanceNotesController.cs
@@ -15,10 +15,23 @@ namespace eTimeTrack.Controllers
         {
             int ProjectId = (int?)Session?["SelectedProject"] ?? 0;
 
+            ViewBag.InfoMessage = TempData["InfoMessage"];
             return View(new ProjectGuidanceNotes
             {
                 ProjectId = ProjectId
             });
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProjectGuidanceNotes notes)
+        {
+            notes.LastModifiedBy = UserHelpers.GetCurrentUserId();
+            notes.LastModifiedDate = DateTime.UtcNow;
+            Db.ProjectGuidanceNotes.Add(notes);
+            Db.SaveChanges();
+
+            TempData["InfoMessage"] = new InfoMessage(InfoMessageType.Success, "Succesfully Saved Project Guidance Notes");
+            return RedirectToAction("Index");
         }
     }
 }
