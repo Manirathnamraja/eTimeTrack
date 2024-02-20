707bf4134d3c04c4b93222c0f83419026ed630bf
Manirathnam Raja <Manirathnam.Raja@aecom.com>
2024-02-20 17:21:08 +0530

    Guidance updated

--- eTimeTrack/Controllers/GuidanceNotesController.cs
+++ eTimeTrack/Controllers/GuidanceNotesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using eTimeTrack.Helpers;
 using eTimeTrack.Models;
+using eTimeTrack.ViewModels;
 
 namespace eTimeTrack.Controllers
 {
@@ -32,6 +33,17 @@ namespace eTimeTrack.Controllers
 
             TempData["InfoMessage"] = new InfoMessage(InfoMessageType.Success, "Succesfully Saved Project Guidance Notes");
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult GetNotes()
+        {
+            int ProjectId = (int?)Session?["SelectedProject"] ?? 0;
+            var results = Db.ProjectGuidanceNotes.Where(x => x.ProjectId == ProjectId).ToList();
+            return View(new GetNotesViewModel
+            {
+                GuidanceNotes = results
+            });
         }
     }
 }
